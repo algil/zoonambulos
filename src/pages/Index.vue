21ce9bf4c97847f6a9398e7ed0a6371b1814641c
Antonio Gil <antonioluisgil@gmail.com>
2019-04-10 22:36:27 +0200

    Initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,37 @@
+<template>
+  <Layout>
+    <h1 class="logo">
+      <img src="@/assets/images/logo.png" alt="Zoonámbulos">
+    </h1>
+    <div class="illustration">
+      <img class="toroid" src="@/assets/images/toroid.svg" alt="Zoonámbulos">
+      <img class="lights" src="@/assets/images/lights.svg" alt="Zoonámbulos">
+      <img class="eye" src="@/assets/images/eye.svg" alt="Zoonámbulos">
+    </div>
+    <footer>
+      <h2>Síguenos</h2>
+      <div class="social">
+        <a href="#">
+          <img src="@/assets/images/instagram.svg" alt="Zoonámbulos">
+        </a>
+        <a href="#">
+          <img src="@/assets/images/facebook.svg" alt="Zoonámbulos">
+        </a>
+        <a href="#">
+          <img src="@/assets/images/youtube.svg" alt="Zoonámbulos">
+        </a>
+        <a href="#">
+          <img src="@/assets/images/spotify.svg" alt="Zoonámbulos">
+        </a>
+      </div>
+    </footer>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Zoonámbulos"
+  }
+};
+</script>
